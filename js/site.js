9ea9c1eaa68b82fbf64277feb3d90c0e4e187f7e
Henri Vesala <henri.vesala@gmail.com>
2013-10-08 19:02:15 +0300

    optimization

--- js/site.js
+++ js/site.js
@@ -1,13 +1,1 @@
 
-	jQuery(document).ready(function($) {
-
-		    //$('#nav-main').scrollspy()
-		    
-		    // Localscrolling 
-    		$('#nav-main, .brand').localScroll();
-     		$('#news, .container').localScroll();
-
-	});
-	
-	
-
